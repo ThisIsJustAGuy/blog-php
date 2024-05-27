555962bcdab17b53fc67e41c6cea18a1fc408262
ThisIsJustAGuy <bbalint0404@gmail.com>
2024-05-27 12:50:16 +0200

    help

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,10 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 Route::resource('posts', PostController::class);
 
 Route::post('comments/create', [CommentController::class, 'store'])->name('comments.store');
+
+//php artisan make:model BookCategory -crR
+
+//composer require laravel/ui
+//php artisan ui bootstrap --auth
+//npm run build
+//chown -R www-data:www-data /var/www/storage
